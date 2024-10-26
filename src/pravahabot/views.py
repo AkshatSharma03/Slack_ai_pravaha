5dd73198b38060b739918a4ba4a4153956d9cda2
akshat sharma <akshat14@bu.edu>
2024-10-25 21:37:16 -0400

    added functions and modified views.py to echo slack message

--- src/pravahabot/views.py
+++ src/pravahabot/views.py
@@ -1,13 +1,33 @@
 import json
+import requests
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
 
+from pprint import pprint
 import helpers
 
 
 SLACK_BOT_OAUTH_TOKEN =helpers.config('SLACK_BOT_OAUTH_TOKEN', default =None,cast=str)
 # Create your views here.
+def send_message(message, channel_id = None):
+    url = "https://slack.com/api/chat.postMessage"
+    headers ={
+        "Content-Type": "application/json; charset-utf-8",
+        "Authorization": f"Bearer {SLACK_BOT_OAUTH_TOKEN} ",
+        "Accept": "application/json"
+    }
+    data ={
+        "channel": channel_id,
+        "text": message
+    }
+    return requests.post(url, json=data, headers=headers)
+    
+# {
+#   "channel": "YOUR_CHANNEL_ID",
+#   "text": "Hello world :tada:"
+# }
+
 #CSRF Exempt
 @require_POST
 @csrf_exempt
@@ -15,10 +35,10 @@ def slack_events_endpoint(request):
     json_data ={}
     try:
         json_data = json.loads(request.body.decode('utf-8'))
-    except:
+    except Exception as e:
         pass
     data_type = json_data.get('type')
-    print(data_type,json_data)
+    print(data_type,json_data.keys(), json_data)
     allowed_data_type =[
         "url_verification",
         "event_callback"
@@ -32,4 +52,13 @@ def slack_events_endpoint(request):
         if challenge is None:
             return HttpResponse("Not Allowed", status=400)
         return HttpResponse(challenge, status=200)
+    elif data_type =="event_callback":
+        event = json_data.get('event') or {}
+        pprint(event)
+        blocks = event.get('blocks')
+        msg_text = event.get('text')
+        # user_id = event.get('user)
+        channel_id = event.get('channel')
+        r = send_message(msg_text, channel_id=channel_id)
+        return HttpResponse("Success", status=200)
     return HttpResponse("Success", status=200)
